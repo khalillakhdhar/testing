10e05538a94ff69fbd9d97cdb8a52cc3c34c8476
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-12 15:27:13 +0100

    push

--- src/testing/VerifGenerale.java
+++ src/testing/VerifGenerale.java
@@ -1,5 +1,14 @@
 package testing;
 
 public class VerifGenerale {
-
+public boolean verifString(String ch)
+{
+	return !ch.isEmpty();
+	/*
+	 * if(!ch.isEmpty)
+	 * return true
+	 * else
+	 * return false;
+	 */
+	}
 }
